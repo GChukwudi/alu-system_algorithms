8cd6fd9e0563cfd1f36eff3617904d79a522e95a
GChukwudi <g.chukwudi@alustudent.com>
2025-02-07 21:06:37 +0200

    project started

---
+++ huffman_coding/heap/heap_create.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "heap.h"
+
+/**
+ * heap_create - Creates a Heap data structure
+ * @data_cmp: Pointer to a comparison function
+ *
+ * Return: Pointer to the created heap_t structure, or NULL if it fails
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+    heap_t *heap;
+
+    if (!data_cmp)
+        return (NULL);
+
+    heap = malloc(sizeof(heap_t));
+    if (!heap)
+        return (NULL);
+
+    heap->size = 0;
+    heap->data_cmp = data_cmp;
+    heap->root = NULL;
+
+    return (heap);
+}
