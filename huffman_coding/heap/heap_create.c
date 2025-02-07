e961002fe1ef52e670114f9f2b8bdf56d778dd37
GChukwudi <g.chukwudi@alustudent.com>
2025-02-07 21:11:36 +0200

    updated

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -9,18 +9,18 @@
  */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-	heap_t *heap;
+    heap_t *heap;
 
-	if (!data_cmp)
-		return (NULL);
+    if (!data_cmp)
+        return (NULL);
 
-	heap = malloc(sizeof(heap_t));
-	if (!heap)
-		return (NULL);
+    heap = malloc(sizeof(heap_t));
+    if (!heap)
+        return (NULL);
 
-	heap->size = 0;
-	heap->data_cmp = data_cmp;
-	heap->root = NULL;
+    heap->size = 0;
+    heap->data_cmp = data_cmp;
+    heap->root = NULL;
 
-	return (heap);
+    return (heap);
 }
